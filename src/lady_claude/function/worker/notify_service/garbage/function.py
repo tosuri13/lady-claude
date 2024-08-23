663edcb034ca2b8e9ae05cae4028a4c5d4426d8b
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2024-08-24 08:36:12 +0900

    :sparkles: ゴミ出し通知機能を追加 + Connpassイベント通知の停止

---
+++ src/lady_claude/function/worker/notify_service/garbage/function.py
@@ -0,0 +1,38 @@
+import datetime
+
+from lady_claude.common.discord import send_message
+from lady_claude.common.util import get_lady_error_comment
+
+
+def handler(event: dict, context: dict) -> None:
+    try:
+        content = _handle_request()
+        send_message(content)
+
+    except Exception as exception:
+        send_message(get_lady_error_comment(exception))
+
+    return None
+
+
+def _handle_request() -> str:
+    day_texts = {
+        1: (
+            "今日は燃えるものを捨てる日ですわ!!\n"
+            "9時までにお運びになるのを忘れないでくださいまし!!"
+        ),
+        2: (
+            "今日は缶・びん・ペットボトルを捨てる日ですわ!!\n"
+            "9時までにお運びになるのを忘れないでくださいまし!!"
+        ),
+        4: (
+            "今日は燃えるものを捨てる日ですわ!!\n"
+            "9時までにお運びになるのを忘れないでくださいまし!!"
+        ),
+        5: (
+            "今日はダンボールを捨てる日ですわ!!\n"
+            "毎週土曜とは限りませんので、雰囲気で近隣住民の皆様に合わせてくださいまし!!"
+        ),
+    }
+
+    return day_texts[datetime.datetime.now().weekday()]
